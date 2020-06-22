02fd506511b505d715605b8ec740a80b2400af57
Žiga Krašovec <zkrasovec@gmail.com>
2020-06-22 13:29:14 +0200

    Removed color property from logConsoleArt function since it lacks readability if Chrome interface is set to dark mode

--- src/utils/functions/console.ts
+++ src/utils/functions/console.ts
@@ -1,5 +1,5 @@
 export const logConsoleArt = (art: string, size: number = 12): void =>
 	console.log(
 		`%c${art}`,
-		`font-family:monospace;font-size:${size}px;color:#14171A;line-height:1.4`
+		`font-family:monospace;font-size:${size}px;line-height:1.4`
 	);
